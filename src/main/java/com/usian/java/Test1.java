4a4acdc82abd5842b445c54128f7a033f2ca78fe
ik1ro <2071243830@qq.com>
2022-08-12 11:44:08 +0800

    测试分支

--- src/main/java/com/usian/java/Test1.java
+++ src/main/java/com/usian/java/Test1.java
@@ -1,4 +1,23 @@
 package com.usian.java;
 
 public class Test1 {
+
+    public static void main(String[] args) {
+        fun1();
+
+        fun2();
+    }
+
+    public static void fun1(){
+
+        String a = "nimasile";
+        System.out.println(a);
+    }
+
+    public static void fun2(){
+
+        String a = "ni ba ye si le";
+        System.out.println(a);
+
+    }
 }
